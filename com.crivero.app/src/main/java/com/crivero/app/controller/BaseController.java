2bc94242526bdb228640d435c3c4f08feec48595
Carlos Rivero Molina <criverom@usersad.everis.int>
2016-10-03 10:26:05 +0200

    Added app in repository

---
+++ com.crivero.app/src/main/java/com/crivero/app/controller/BaseController.java
@@ -0,0 +1,61 @@
+package com.crivero.app.controller;
+
+import java.util.Locale;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.crivero.arq.module.configuration.AppProperties;
+import com.crivero.arq.module.configuration.domain.Application;
+import com.crivero.arq.module.literals.LiteralsProvider;
+
+@Controller
+public class BaseController {
+
+	private static final String VIEW_INDEX = "index";
+	final static Logger logger = LogManager.getLogger(BaseController.class.getName());
+
+	@Autowired
+	private Application application;
+
+	@Autowired
+	private AppProperties appProperties;
+	
+	@Autowired
+	private LiteralsProvider literalsProvider;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+
+		String msg = "";
+		
+		String literal1 = literalsProvider.getLiteral("customer.name", new Object[] { 28, "google.com" }, Locale.US);		
+		String literal2 = literalsProvider.getLiteral("customer.namer", new Object[] { 28, "google.com" }, new Locale("ES"));
+		String property1 = appProperties.getProperty("hello.url");
+		String property2 = appProperties.getProperty("hello.db");
+		String property3 = appProperties.getProperty("error.lalala");
+
+		msg = "Simple bean: " + application + "\n";
+		model.addAttribute("message1", msg);
+		msg = "Property from a file: " +  property1+ "\n";
+		model.addAttribute("message2", msg);
+		msg = "Propery from default: " + property2 + "\n";
+		model.addAttribute("message3", msg);
+		msg = "Propery not found: " + property3 + "\n";
+		model.addAttribute("message4", msg);
+		msg = "Literal: "+ literal1 + "\n";
+		model.addAttribute("message5", msg);
+		msg = "Literal not found: "+ literal2 + "\n";
+		model.addAttribute("message6", msg);
+
+		logger.debug("debug");
+
+		return VIEW_INDEX;
+
+	}
+}
--- com.crivero.arq.parent/com.crivero.arq.module.root/src/main/java/com/crivero/arq/module/root/configuration/AppConfig.java
+++ com.crivero.arq.parent/com.crivero.arq.module.root/src/main/java/com/crivero/arq/module/root/configuration/AppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan({ "com.crivero.arq.module", "com.carlos" })
+@ComponentScan({ "com.crivero" })
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
 
