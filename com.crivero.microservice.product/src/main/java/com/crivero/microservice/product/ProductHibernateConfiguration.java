cbdd5493f40148bc274374d01f64d34df12f964c
Carlos Rivero Molina <criverom@usersad.everis.int>
2016-12-14 13:47:22 +0000

    updated credentials declaration

--- com.crivero.microservice.product/src/main/java/com/crivero/microservice/product/ProductHibernateConfiguration.java
+++ com.crivero.microservice.product/src/main/java/com/crivero/microservice/product/ProductHibernateConfiguration.java
@@ -2,7 +2,14 @@ package com.crivero.microservice.product;
 
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.util.Properties;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import javax.sql.DataSource;
 
@@ -13,27 +20,52 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 public class ProductHibernateConfiguration {
 
-	private boolean local = false;
-	private String pcf_hostname = "us-cdbr-iron-east-04.cleardb.net";
-	private String pcf_port = "3306";
-	private String pcf_username = "bc7f8a08fdca6e";
-	private String pcf_password = "1e12f674";
-	private String pcf_schema = "ad_1fa92171941f578";
+	private String hostname;
+	private String port;
+	private String username;
+	private String password;
+	private String name;
+
+	private void updateCredentials() {
+		String vcap_services = System.getenv("VCAP_SERVICES");
+		if (vcap_services != null && vcap_services.length() > 0) {
+			JsonObject obj = (JsonObject) new JsonParser().parse(vcap_services);
+			Entry<String, JsonElement> dbEntry = null;
+			Set<Entry<String, JsonElement>> entries = obj.entrySet();
+
+			for (Entry<String, JsonElement> eachEntry : entries) {
+				if (eachEntry.getKey().equals("cleardb")) {
+					dbEntry = eachEntry;
+					break;
+				}
+			}
+			if (dbEntry == null) {
+				throw new RuntimeException("Could not find cleardb key in VCAP_SERVICES env variable");
+			}
+			obj = (JsonObject) ((JsonArray) dbEntry.getValue()).get(0);
+			JsonObject credentials = (JsonObject) obj.get("credentials");
+			hostname = credentials.get("hostname").getAsString();
+			port = credentials.get("port").getAsString();
+			username = credentials.get("username").getAsString();
+			password = credentials.get("password").getAsString();
+			name = credentials.get("name").getAsString();
+		} else {
+			hostname = "localhost";
+			port = "3306";
+			username = "root";
+			password = "1234";
+			name = "world";
+		}
+	}
 
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dmds = new DriverManagerDataSource();
+		updateCredentials();
 		dmds.setDriverClassName("com.mysql.jdbc.Driver");
-		if (local) {
-			dmds.setUrl("jdbc:mysql://localhost:3306/world?useSSL=false&serverTimezone=UTC");
-			dmds.setUsername("root");
-			dmds.setPassword("1234");
-		} else {
-			dmds.setUrl("jdbc:mysql://" + pcf_hostname + ":" + pcf_port + "/" + pcf_schema
-					+ "?useSSL=false&serverTimezone=UTC");
-			dmds.setUsername(pcf_username);
-			dmds.setPassword(pcf_password);
-		}
+		dmds.setUrl("jdbc:mysql://" + hostname + ":" + port + "/" + name + "?useSSL=false&serverTimezone=UTC");
+		dmds.setUsername(username);
+		dmds.setPassword(password);
 		return dmds;
 	}
 
@@ -42,7 +74,7 @@ public class ProductHibernateConfiguration {
 		Properties properties = new Properties();
 		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
 		properties.put("hibernate.show_sql", "true");
-		properties.put("hibernate.hbm2ddl.auto", "create");
+		properties.put("hibernate.hbm2ddl.auto", "validate");
 		return properties;
 	}
 
